b50c4d2ee910c7365fc102eaee4a019790386b2c
Pass80 <ferassaleh1980@gmail.com>
2023-03-21 19:03:45 +0100

    adding nav compoonent

--- src/App.js
+++ src/App.js
@@ -24,6 +24,22 @@ function App() {
           </Form>
         </Navbar.Collapse>
       </Navbar>
+      <Nav justify variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled">
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
 
 
       <Container>
